0fff145e3bc498e2b7e1565da923e278047a16e6
Malik Johnson <malikjohnson159@yahoo.com>
2025-01-21 11:30:00 -0500

    Add any and all examples

--- python practice 2.py
+++ python practice 2.py
@@ -66,3 +66,8 @@ print(squared)
 numbers = [1, 2, 3, 4, 5, 6]
 evens = list(filter(lambda x: x % 2 == 0, numbers))
 print(evens)
+
+#Learning about any and all
+nums = [2, 4, 6]
+print(any(n % 2 != 0 for n in nums))  # False
+print(all(n % 2 == 0 for n in nums))  # True
