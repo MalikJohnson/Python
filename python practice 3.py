6e29d24772dbb89a541ab6215c84a85b24f2d0ed
Malik Johnson <malikjohnson159@yahoo.com>
2025-02-02 09:30:00 -0500

    Add palindrome check function

--- python practice 3.py
+++ python practice 3.py
@@ -3,3 +3,10 @@ def reverse_string(s):
     return s[::-1]
 
 print(reverse_string("hello"))
+
+#another palindrome check using reverse
+def is_palindrome(s):
+    return s == s[::-1]
+
+print(is_palindrome("racecar"))
+print(is_palindrome("hello"))
