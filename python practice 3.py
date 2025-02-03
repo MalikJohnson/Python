2daab1fa44f883eceec27e305d47d47f7851d0fc
Malik Johnson <malikjohnson159@yahoo.com>
2025-02-03 10:05:00 -0500

    Add two sum function

--- python practice 3.py
+++ python practice 3.py
@@ -10,3 +10,15 @@ def is_palindrome(s):
 
 print(is_palindrome("racecar"))
 print(is_palindrome("hello"))
+
+#another twosum test this time with python and using hashmap
+def two_sum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in hashmap:
+            return [hashmap[complement], i]
+        hashmap[num] = i
+    return []
+
+print(two_sum([2,7,11,15], 9))
