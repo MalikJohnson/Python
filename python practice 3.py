24a84615c0da0cb4c1826602c613cde56c5206e1
Malik Johnson <malikjohnson159@yahoo.com>
2025-02-05 09:15:00 -0500

    Add merge sorted arrays function

--- python practice 3.py
+++ python practice 3.py
@@ -28,3 +28,9 @@ def remove_duplicates(nums):
     return list(set(nums))
 
 print(remove_duplicates([1,2,2,3,4,4,5]))
+
+#merge two sorted arays
+def merge_sorted(arr1, arr2):
+    return sorted(arr1 + arr2)
+
+print(merge_sorted([1,3,5], [2,4,6]))
