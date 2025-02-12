cb8df6875a14f89dd0448fbe2a7c2c81ae0bd940
Malik Johnson <malikjohnson159@yahoo.com>
2025-02-12 09:10:00 -0500

    Add fizzbuzz implementation

--- python practice 3.py
+++ python practice 3.py
@@ -82,3 +82,17 @@ def first_non_repeating_char(s):
 
 print(first_non_repeating_char("leetcode"))
 print(first_non_repeating_char("aabbcc"))
+
+#classic fizzbuzz
+def fizzbuzz(n):
+    for i in range(1, n+1):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz(15)
