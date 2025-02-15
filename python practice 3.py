cf7c9a6f619d297d9f42cee7f8c7479dda3114b6
Malik Johnson <malikjohnson159@yahoo.com>
2025-02-15 09:40:00 -0500

    Add linear search implementation

--- python practice 3.py
+++ python practice 3.py
@@ -117,3 +117,13 @@ def is_balanced(s):
 
 print(is_balanced("()[]{}"))
 print(is_balanced("([)]"))
+
+#linear bad search
+def linear_search(arr, target):
+    for i, val in enumerate(arr):
+        if val == target:
+            return i
+    return -1
+
+print(linear_search([1,2,3,4,5], 3))
+print(linear_search([1,2,3,4,5], 6))
