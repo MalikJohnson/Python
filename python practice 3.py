660c1b719043e138c45beb98436c931a814f5bc8
Malik Johnson <malikjohnson159@yahoo.com>
2025-02-16 10:45:00 -0500

    Add max profit stock prices function

--- python practice 3.py
+++ python practice 3.py
@@ -127,3 +127,16 @@ def linear_search(arr, target):
 
 print(linear_search([1,2,3,4,5], 3))
 print(linear_search([1,2,3,4,5], 6))
+
+#find max profit from stock prices
+def max_profit(prices):
+    min_price = float('inf')
+    max_profit = 0
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+    return max_profit
+
+print(max_profit([7,1,5,3,6,4]))
