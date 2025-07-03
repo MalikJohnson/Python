02ed00affc074c7bb96bd29fa5111a2e95cc670f
Malik Johnson <malikjohnson159@yahoo.com>
2025-07-03 17:33:00 -0400

    Add sqrt_finder.py

---
+++ sqrt_finder.py
@@ -0,0 +1,9 @@
+import math
+
+def find_square_root(number):
+    return math.sqrt(number)
+
+if __name__ == "__main__":
+    print(find_square_root(4))
+    print(find_square_root(9))
+    print(find_square_root(16))
